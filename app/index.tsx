b871985f695dd16ccbfb7ed7a0eca4bac679805a
Sarcastic-Soul <anishisbusy@gmail.com>
2025-02-14 00:32:49 +0530

    initialize project structure with basic layout and configuration files

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+// web: 1001660968412-3ud3ip5sjr8um872kgcha5fl84l0pmi1.apps.googleusercontent.com
+// ios: 1001660968412-lgumju7c6ubusi97ejarl7pk42oqrkfk.apps.googleusercontent.com
+// android: 
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
